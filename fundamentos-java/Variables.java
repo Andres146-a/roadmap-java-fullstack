d72d1351211543f53adb0c99e1088d7bce4f53a4
Andres146-a <matyascanola906@gmail.com>
2026-02-03 23:55:52 -0500

    Subida de archivo iniciales de prueba

---
+++ fundamentos-java/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+        int edad = 20; 
+        double promedio = 8.5;
+        String nombre = "Mi Primer Proyecto";
+
+        System.out.println("Edad: " + edad);
+        System.out.println("Promedio: " + promedio);
+        System.out.println("Nombre del Proyecto: " + nombre);
+    }
+}
