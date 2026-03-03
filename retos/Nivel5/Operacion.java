a09b00d0c5bad8b1008691a9cc372ae340b36407
Andres146-a <matyascanola906@gmail.com>
2026-03-03 15:10:53 -0500

    Agregar reto Nivel 5: BancoServicio y clases Usuario, Cuenta, Operacion

---
+++ retos/Nivel5/Operacion.java
@@ -0,0 +1,23 @@
+package Nivel5;
+
+public class Operacion {
+    String tipo; 
+    double monto;
+    public Operacion(String tipo, double monto) {
+        this.tipo = tipo;
+        this.monto = monto;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    public double getMonto() {
+        return monto;
+    }
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+    
+}
